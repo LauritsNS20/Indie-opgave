4da3ec246528f831db714ccb0eb5bb58bf6a2994
rts-6905 <U2418040@rts-365.dk>
2023-06-08 13:06:22 +0200

    komentarer

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,17 +1,17 @@
-//
+// here caller vi på hvors elementer med classer/  IKKE SE BARE CLASSER
 const IMAGE_SLIDERS = document.querySelector(".image-sliders")
 const BUTTONS_FOR_SLIDER = document.querySelectorAll(".buttons-for-slider")
 
+
 //document.addEventListener("click",goToNexstSliders)
-
-
+//here for jeg buttonen til at kunne klike og gør at buttonsne virkere/ here kaller vi på hvors function med index
 BUTTONS_FOR_SLIDER.forEach((button, index) => button.addEventListener("click", () => goToNextSliders(index)))
 
 
 
 
 
-//
+// here har vi hvors function 
 function goToNextSliders(buttonIndex){
     const SLIDERS_WIDTH = IMAGE_SLIDERS.getBoundingClientRect().width 
     console.log(SLIDERS_WIDTH)
